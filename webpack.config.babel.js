d5e503a072dedbf732bd05431aedaee7553f3cf3
Ezra Raez <ezRAez10@gmail.com>
2016-12-23 19:44:26 -0800

    add live reload

---
+++ webpack.config.babel.js
@@ -0,0 +1,62 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  inject: "body"
+});
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist')
+};
+
+const LAUNCH_COMMAND = process.env.npm_lifecycle_event;
+
+const base = {
+  entry: [
+    PATHS.app
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
+    ]
+  },
+  resolve: {
+    root: path.resolve('./app')
+  }
+}
+
+const developmentConfig = {
+  devtool: "cheap-module-inline-source-map",
+  devServer: {
+    contentBase: PATHS.build,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [HtmlWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
+};
+
+const productionPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+});
+
+const productionConfig = {
+  devtool: "cheap-module-source-map",
+  plugins: [HtmlWebpackPluginConfig, productionPlugin]
+};
+
+const isProduction = LAUNCH_COMMAND === "production";
+process.env.BABEL_ENV = LAUNCH_COMMAND;
+
+export default Object.assign({}, base,
+  isProduction === true ? productionConfig : developmentConfig);
